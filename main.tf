0a02cf2d233d7199ef08d5d128d5cb3ef4eed59d
OravlaZinum <alvroox@gmail.com>
2023-11-10 16:09:51 +0100

    TF TEST

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone = var.zone
+}
+
+#VPC
+resource "google_compute_network" "vpc_network" {
+  name = "my-terraform-network"
+}
+
+#Virtual Machine
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-tf-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
